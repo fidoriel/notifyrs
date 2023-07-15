d55a59c3eed866f937aebd2cdc923623070bc4df
fidoriel <49869342+fidoriel@users.noreply.github.com>
2023-07-15 13:03:27 +0200

    notifyrs

--- readme.md
+++ readme.md
@@ -1,6 +1,18 @@
-# rs_api
+# notifyrs
+WARNING: This is not ment to be hosted publicly for various reasons.
 
-Very simple api sometimes a bit useless.
+A very simple rust api where you can throw an json on and send an email. This is a micro service for your homeserver so you dont have to update passwords everywhere, just send notifications. There is ONE optional API Token, you may use it or not.
 
+Test with Curl (note: you may update the target)
+```bash
+curl -X POST -H "Content-Type: application/json" -d '{"topic": "Hello, World!", "body": "This is the body of the email."}' http://0.0.0.0:8080/notify
+```
+With the `recipient_email` key in the post you can provide a list with recipients. If not set, it will default to the default the corresponding env var.
+
+For use with the token append
+```bash
+-H "X-SECRET-KEY: 1234567890"
+```
+## dev
 run: `cargo run`
 format: `cargo fmt`
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,109 @@
-use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
-use rand::Rng;
+use actix_web::{post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use lettre::{message::Mailbox, Message, SmtpTransport, Transport};
+use serde::Deserialize;
+use std::env;
 
-async fn success_or_not(prob: f64) -> bool {
-    let mut rng = rand::thread_rng();
-    let x: f64 = rng.gen();
-    x < prob
+// Define our data structures
+#[derive(Deserialize)]
+struct EmailInput {
+    recipient_email: Option<Vec<String>>,
+    topic: String,
+    body: String,
 }
 
-#[post("/random_echo")]
-async fn random_echo(req_body: String) -> impl Responder {
-    let prob = 0.99;
-    let success = success_or_not(prob).await;
-    println!("Success: {} Request body: {}", success, req_body);
-    if success {
-        HttpResponse::Ok().body(req_body)
-    } else {
-        HttpResponse::BadRequest().body("Bad request")
+struct Config {
+    sender_email: String,
+    username: String,
+    password: String,
+    default_recipient_emails: Vec<String>,
+    smtp_host: String,
+    secret_key: Option<String>,
+}
+
+#[post("/notify")]
+async fn send_email(req: HttpRequest, info: web::Json<EmailInput>) -> impl Responder {
+    let config = match load_config() {
+        Ok(config) => config,
+        Err(err) => return HttpResponse::InternalServerError().body(err.to_string()),
+    };
+
+    if let Some(secret_key) = config.secret_key {
+        match req.headers().get("X-SECRET-KEY") {
+            Some(value) => {
+                if value.to_str().unwrap_or("") != secret_key {
+                    return HttpResponse::Unauthorized().body("Invalid secret key");
+                }
+            }
+            None => return HttpResponse::Unauthorized().body("No secret key provided"),
+        };
     }
+
+    let recipients = match &info.recipient_email {
+        Some(emails) => emails.clone(),
+        None => config.default_recipient_emails.clone(),
+    };
+
+    let mut response = Vec::new();
+
+    for recipient_email in recipients {
+        let email = Message::builder()
+            .from(Mailbox::new(
+                None,
+                config.sender_email.clone().parse().unwrap(),
+            ))
+            .to(Mailbox::new(None, recipient_email.clone().parse().unwrap()))
+            .subject(info.topic.clone())
+            .body(info.body.clone())
+            .unwrap();
+
+        let mailer = SmtpTransport::relay(&config.smtp_host)
+            .unwrap()
+            .credentials(lettre::transport::smtp::authentication::Credentials::new(
+                config.username.clone(),
+                config.password.clone(),
+            ))
+            .build();
+
+        match mailer.send(&email) {
+            Ok(_) => response.push(format!("Email sent to {}", recipient_email)),
+            Err(e) => response.push(format!(
+                "Failed to send email to {}: {}",
+                recipient_email, e
+            )),
+        }
+    }
+    HttpResponse::Ok().body(format!("{:?}", response))
 }
 
-#[post("/post")]
-async fn simple_post(req_body: String) -> impl Responder {
-    println!("Request body: {}", req_body);
-    HttpResponse::Ok().body(req_body)
-}
+fn load_config() -> Result<Config, String> {
+    let sender_email =
+        env::var("SENDER_EMAIL").map_err(|_| "SENDER_EMAIL environment variable not set")?;
+    let username =
+        env::var("SMTP_USERNAME").map_err(|_| "SMTP_USERNAME environment variable not set")?;
+    let password =
+        env::var("SMTP_PASSWORD").map_err(|_| "SMTP_PASSWORD environment variable not set")?;
+    let default_recipient_emails = env::var("DEFAULT_RECIPIENT_EMAILS")
+        .map(|emails| emails.split(',').map(String::from).collect())
+        .map_err(|_| "DEFAULT_RECIPIENT_EMAILS environment variable not set or invalid format")?;
+    let smtp_host = env::var("SMTP_HOST").map_err(|_| "SMTP_HOST environment variable not set")?;
+    let secret_key = env::var("SECRET_KEY").ok();
 
-#[get("/health")]
-async fn health() -> impl Responder {
-    HttpResponse::Ok().body("OK")
+    Ok(Config {
+        sender_email,
+        username,
+        password,
+        default_recipient_emails,
+        smtp_host,
+        secret_key,
+    })
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(random_echo)
-            .service(health)
-            .service(simple_post)
-    })
-    .bind(("0.0.0.0", 8080))?
-    .run()
-    .await
+    dotenv().ok();
+    HttpServer::new(move || App::new().service(send_email))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
 }
